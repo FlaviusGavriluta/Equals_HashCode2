9a556b36030f04db68e373cde9e03132bbd5b7dd
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-09-27 05:15:30 +0300

    find a solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
 package org.example;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<Student, String> studentMap = new HashMap<>();
+        studentMap.put(new Student(1, "s1", 18), "Rec1");
+        studentMap.put(new Student(2, "s2", 21), "Rec2");
+        studentMap.put(new Student(3, "s3", 17), "Rec3");
+
+        System.out.println("size: " + studentMap.size());
+        System.out.println("student: " + studentMap);
+
+        TreeSet<Student> studentTreeSet = new TreeSet<>();
+        studentTreeSet.add(new Student(1, "s1", 18));
+        studentTreeSet.add(new Student(2, "s3", 22));
+        studentTreeSet.add(new Student(3, "s2", 17));
+
+        System.out.println("size studentTreeSet: " + studentTreeSet.size());
+        System.out.println("studentsTreeSet: " + studentTreeSet);
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+    private int id;
+    private String name;
+    private Integer age;
+
+    public Student(int id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age.equals(student.getAge());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.age.compareTo(o.getAge());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
